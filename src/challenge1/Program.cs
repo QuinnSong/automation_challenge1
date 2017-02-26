b4599de12bd1c3ada3837dbd4eaae0b672a6806e
QuinnSong <ccf-midi@hotmail.com>
2017-02-25 20:40:20 -0500

    add screenshot to html report

--- src/challenge1/Program.cs
+++ src/challenge1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.Linq;
 using RelevantCodes.ExtentReports;
 using OpenQA.Selenium;
@@ -11,8 +12,10 @@ namespace Challenge1
     internal class Program
     {
         private static ExtentReports _extent;
+        private static ExtentTest _test;
         private static readonly IWebDriver Driver = new InternetExplorerDriver();
         private static readonly string reportLocation = @"e:\challenge1_report.html";
+        private static readonly string screenshotLocation = @"e:\challenge1_screenshot.png";
         private static readonly string baseUrl = @"<source website for testing>";
 
         private static void Main()
@@ -20,32 +23,47 @@ namespace Challenge1
             // init extent reports
             _extent = new ExtentReports(reportLocation, DisplayOrder.NewestFirst);
             _extent.LoadConfig("extent-config.xml");
-            var test = _extent.StartTest("Challenge 1", "This is the challenge 1 test");
-            var optionVotes = RunTest(test);
+            _test = _extent.StartTest("Challenge 1", "This is the challenge 1 test");
+            var optionVotes = RunTest(_test);
             if (optionVotes != string.Empty)
             {
-                test.Log(LogStatus.Pass, "number of people that have voted for that same option: " + Convert.ToInt16(optionVotes));
+                _test.Log(LogStatus.Pass, "people voted for the same option: " + Convert.ToInt16(optionVotes));
             }
             else
             {
-                test.Log(LogStatus.Fail, "number of people that have voted for that same option: --");
+                _test.Log(LogStatus.Fail, "people voted for the same option: --");
             }
-            _extent.EndTest(test);
+            _extent.EndTest(_test);
             _extent.Flush();
-
+            _extent.Close();
             Driver.Quit();
 
+        }
+
+        private static void TakeScreenshot(IWebDriver driver, string fileName)
+        {
+            ITakesScreenshot screenshotDriver = driver as ITakesScreenshot;
+            if (screenshotDriver == null)
+            {
+                _test.Log(LogStatus.Fail, "Error in taking screenshot!");
+            }
+            else
+            {
+                Screenshot screenshot = screenshotDriver.GetScreenshot();
+                screenshot.SaveAsFile(fileName, ImageFormat.Png);
+            }
+            
         }
 
         private static string RunTest(ExtentTest test)
         {
             Driver.Url = baseUrl;
-            AssertTitle("Slashdot: News for nerds, stuff that matters", test);
+            AssertTitle("stuff that matters", test);
             try
             {
 
                 var articles = Driver.FindElements(By.CssSelector(@"span[class='story-title']"));
-                test.Log(LogStatus.Info, "Print how many articles are on the page ", articles.Count.ToString());
+                test.Log(LogStatus.Info, "Log articles qty on the page ", articles.Count.ToString());
                 var allIcons = Driver.FindElements(By.CssSelector(@"span[class='topic'] > a >img"));
                 List<string> allTitles =
                     allIcons.ToList().ConvertAll(x => x.GetAttribute("title")).ToList();
@@ -55,32 +73,34 @@ namespace Challenge1
                     select new { value = t.Key, count = t.Count() };
                 foreach (var t in uniqueIcons)
                 {
-                    // Print a list of unique (different) icons used on article titles and how many times was it used 
+                    // Log unique icons and used count 
                     test.Log(LogStatus.Info, "Unique icon title: " + t.value, "Used times: " + t.count.ToString());
                 }
 
                 IWebElement form = Driver.FindElement(By.Id("pollBooth"));
                 IWebElement[] labels = Driver.FindElements(By.CssSelector(@"form#pollBooth>label")).ToArray();
 
-                // pickup a random option (label)
+                // pickup a random label
                 Random rand = new Random();
                 int index = rand.Next(0, labels.Length);
 
                 labels[index].Click();
                 string optionText = labels[index].Text;
-                // Vote for some random option on the daily poll 
+                // Vote a random option
                 form.Submit();
 
                 WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(3));
                 wait.Until(d => d.FindElement(By.CssSelector("div[class='poll-bar-label']")));
 
-                AssertTitle("Slashdot Poll | I think my current job will ...", test);
+                AssertTitle("humans discover life on another ...", test);
                 string[] labelTexts = Driver.FindElements(By.CssSelector("div[class='poll-bar-label']")).ToList().ConvertAll(x => x.Text).ToArray();
                 string[] labelVotes = Driver.FindElements(By.CssSelector("div[class='poll-bar-group']>div>div>div")).ToList().ConvertAll(x => x.Text.Split()[0]).ToArray();
 
                 string optionVotes = labelVotes[Array.IndexOf(labelTexts, optionText)];
+                TakeScreenshot(Driver, screenshotLocation);
+                _test.Log(LogStatus.Info, "Screenshot - " + _test.AddScreenCapture(screenshotLocation)); // add screenshot
 
-                // Return the number of people that have voted for that same option 
+                // Return total # people for the same option 
                 return optionVotes;
             }
             catch (Exception e)
@@ -93,10 +113,8 @@ namespace Challenge1
 
         private static void AssertTitle(string expectedTitle, ExtentTest test)
         {
-            if (Driver.Title == expectedTitle)
-            {
-                test.Log(LogStatus.Pass, "Check page title", "Page title is: " + expectedTitle);
-            }
+            if (Driver.Title.EndsWith(expectedTitle))
+                test.Log(LogStatus.Pass, "Check page title", "Page title was verified");
             else
             {
                 test.Log(LogStatus.Fail, "Check page title", "Incorrect page title" + Driver.Title);
